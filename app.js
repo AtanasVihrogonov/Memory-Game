9a9c2e71007b0fd76b8d18ef39b9cc89a2e93065
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-04-14 15:30:34 +0100

    Create the board

---
+++ app.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Card options
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+  ]
+
+  const grid = document.querySelector('.grid');
+
+  // Create the board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      // Create the image element for each card
+      let card = document.createElement('img');
+      // Set atributes
+      card.setAttribute('src', 'images/blank.png');
+      card.setAttribute('data-id', i);
+
+      // Add event listener for flipcard
+      //card.addEventListener('click', flipcard)
+
+      grid.appendChild(card)
+    }
+  }
+
+  // Check for matches
+
+  // Flip the card
+  function flipcard() {
+    
+  }
+
+  createBoard();
+})
